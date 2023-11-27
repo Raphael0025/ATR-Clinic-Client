7077f45e66a99930796a71ee34998d7d17d5d18f
Raphael Isla <rpbisla0025@gmail.com>
2023-11-27 16:03:46 +0800

    bulk

--- clinic/src/App.js
+++ clinic/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { PageRoute, AdminRoute, SingleView } from 'View';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { CartProvider } from 'Context/CartContext'
+import { ItemsProvider } from 'Context/ItemsContext'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <CartProvider>
+        <ItemsProvider>
+          <div className="App w-auto">
+            <Routes>
+              <Route path='/*' element={<PageRoute />} />
+              <Route path='/admin/*' element={<AdminRoute />} />
+              <Route path='/confirmation/*' element={<SingleView />} />
+            </Routes>
+          </div>
+        </ItemsProvider>
+      </CartProvider>
+    </BrowserRouter>
   );
 }
 
