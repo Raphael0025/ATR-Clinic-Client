efdc927cbfac529d0aa0e3d5017b3cdbd4cc466a
Raphael Isla <rpbisla0025@gmail.com>
2023-11-28 03:14:33 +0800

    rm

--- clinic/src/Components/Modals/AddToCart.jsx
+++ clinic/src/Components/Modals/AddToCart.jsx
@@ -39,7 +39,7 @@ const AddToCart = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         // Add your form submission logic here using formData
-        const response = await fetch('/api/cart', {
+        const response = await fetch('https://clinic-api-two.vercel.app/api/cart', {
             method: 'POST',
             body: JSON.stringify(item),
             headers: {
